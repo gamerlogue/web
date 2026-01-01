008735126b221c9c701d6444b89ff5881be0a7f8
Maicol Battistini <maicolbattistini@live.it>
2026-01-01 15:14:01 +0100

    style(tests): 🎨 Improve formatting in IgdbProxyTest
    These minor adjustments enhance code readability and consistency. Removing an unnecessary empty line reduces visual clutter, and standardizing spacing around the concatenation operator aligns with common PHP style guidelines.

--- tests/Feature/IgdbProxyTest.php
+++ tests/Feature/IgdbProxyTest.php
@@ -7,7 +7,6 @@ test('returns error if not configured', function () {
     config()->set('igdb.credentials.access_token');
     Cache::flush();
 
-
     $this->post('/api/igdb/games', ['fields id; where id = 1;'])
 //        ->dump()
         ->assertStatus(500);
@@ -24,5 +23,5 @@ test('caches successful get response', function () {
 //        ->dump()
         ->assertJsonCount($count);
 
-    $this->assertTrue(Cache::has(config('igdb.cache_prefix', 'igdb_cache') . '.' . md5('games' . $query)));
+    $this->assertTrue(Cache::has(config('igdb.cache_prefix', 'igdb_cache').'.'.md5('games'.$query)));
 });
