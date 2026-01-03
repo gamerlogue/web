f6992aa3dd292872ef65ead9fb7c1d3241807ba8
Maicol Battistini <maicolbattistini@live.it>
2026-01-03 22:07:06 +0100

    refactor(locale): 🔧 Use LaravelLang to retrieve available locales
    This change replaces the manual file system scan for available locales with the `LaravelLang\Locales` package. This provides a more robust and reliable method for discovering installed languages, ensuring better consistency and reducing the potential for errors compared to directly checking JSON files in the `lang` directory. It leverages a dedicated package designed for locale management within Laravel, offering a more maintainable approach.

--- app/Http/Middleware/LocaleMiddleware.php
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -5,6 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use LaravelLang\Locales\Data\LocaleData;
+use LaravelLang\Locales\Facades\Locales;
 use Symfony\Component\HttpFoundation\Response;
 
 class LocaleMiddleware
@@ -13,10 +16,9 @@ class LocaleMiddleware
     {
         $available_locales = cache()->rememberForever(
             'available_locales',
-            static fn () => array_map(
-                static fn (string $path) => basename($path, '.json'),
-                glob(lang_path('*.json'), GLOB_NOSORT)
-            )
+            static fn () =>
+            /** @var Collection<LocaleData> $langs */
+            Locales::available()->map(fn (LocaleData $lang) => $lang->locale->value)
         );
         $locale = $request->session()->get('locale', $request->getPreferredLanguage($available_locales));
         if (is_string($locale)) {
