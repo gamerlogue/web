dd02125baccdc17d6ccf496421d109f64cbc9d0c
Maicol Battistini <m.battistini@trust-itservices.com>
2026-07-30 15:59:50 +0200

    chore(script): 🔧 enhance log output and add pail process
    - Updated log colors to include magenta for improved visibility.
    - Added `php artisan pail` command for streamlined local development.
    - Adjusted process names to improve clarity in concurrent tasks.

--- deployment/dev/start-container-dev.sh
+++ deployment/dev/start-container-dev.sh
@@ -74,8 +74,9 @@ if [ ! -d "$APP_BASE_DIR/node_modules" ]; then
 fi
 
 unbuffer bunx concurrently \
-    -c "red.bold,yellow.bold" \
+    -c "red.bold,magenta.bold,yellow.bold" \
   "unbuffer php $PHP_INI_FLAGS $ARTISAN octane:start --host=$SERVER_NAME --port=$PORT $HTTPS --server=frankenphp --admin-port=$CADDY_ADMIN_PORT $EXTRA_OCTANE_FLAGS --caddyfile=/etc/frankenphp/Caddyfile" \
+  "php artisan pail --timeout=0" \
   "unbuffer bun dev" \
-  --names=server,vite \
+  --names=server,logs,vite \
   --kill-others
